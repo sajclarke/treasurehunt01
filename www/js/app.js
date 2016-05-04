b357bf213d973dfd8a3bf281dd5e9b82a946db74
denzildoyle <yell@denzildoyle.me>
2016-05-04 14:00:52 -0400

    Format map page removing unnecessary spacing

--- www/js/app.js
+++ www/js/app.js
@@ -40,7 +40,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
-
     }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
